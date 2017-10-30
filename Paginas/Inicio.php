008e15295b536a4bab0bd2e7d8c65f8e6826bc3c
IsaiRamirezG <33229927+isairamirezg@users.noreply.github.com>
2017-10-30 11:27:22 -0600

    Primer Commit

---
+++ Paginas/Inicio.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+
+    <title>Starter Template for Bootstrap</title>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <style>
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #555;
+            color: white;
+            padding: 15px;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Inicio</a></li>
+                <li><a href="#">Mensajes</a></li>
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group input-group">
+                    <input type="text" class="form-control" placeholder="Search..">
+                    <span class="input-group-btn">
+            <button class="btn btn-default" type="button">
+              <span class="glyphicon glyphicon-search"></span>
+            </button>
+          </span>
+                </div>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Mi cuenta</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<div class="container text-center">
+    <div class="row">
+        <div class="col-sm-3 well">
+            <div class="well">
+                <p><a href="#">Mi perfil</a></p>
+                <img src="../Imagenes/photo-99135_960_720.png" class="img-circle" height="65" width="65" alt="Avatar">
+            </div>
+            <div class="well">
+                <p><a href="#">Podria interesarte</a></p>
+                <p>
+                    <span class="label label-default">Novedades</span>
+                    <span class="label label-primary">Escuela</span>
+                    <span class="label label-success">Arte</span>
+                    <span class="label label-info">Football</span>
+                    <span class="label label-warning">Videojuegos</span>
+                    <span class="label label-danger">Amigos</span>
+                </p>
+            </div>
+            <div class="alert alert-success fade in">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+                <p><strong>Ey!</strong></p>
+                Gente esta viendo tu perfil. Encuentra quien.
+            </div>
+            <p><a href="#">Link</a></p>
+            <p><a href="#">Link</a></p>
+            <p><a href="#">Link</a></p>
+        </div>
+        <div class="col-sm-7">
+
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="panel panel-default text-left">
+                        <div class="panel-body">
+                            <p contenteditable="true">Stado</p>
+                            <button type="button" class="btn btn-default btn-sm">
+                                <span class="glyphicon glyphicon-thumbs-up"></span> Me gusta
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="well">
+                        <p>Leyva</p>
+                        <img src="../Imagenes/Netero.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+                    </div>
+                </div>
+                <div class="col-sm-9">
+                    <div class="well">
+                        <p>Soy programador y diseñador digital profesional. Mi especialidad es crear videojuegos en C# y a veces diseño en 3D. Estoy pensando seriamente en abrir un estudio con mis amigos...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="well">
+                        <p>Isaí</p>
+                        <img src="../Imagenes/498904-naughty-dog-pensaban-que-last-us-iba-ser-fracaso.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+                    </div>
+                </div>
+                <div class="col-sm-9">
+                    <div class="well">
+                        <p>Soy programador y diseñador digital profesional. Mi especialidad es crear videojuegos en C# y a veces diseño en 3D. Estoy pensando seriamente en abrir un estudio con mis amigos...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="well">
+                        <p>Orlando</p>
+                        <img src="../Imagenes/Battlefield1.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+                    </div>
+                </div>
+                <div class="col-sm-9">
+                    <div class="well">
+                        <p>Soy programador y diseñador digital profesional. Mi especialidad es crear videojuegos en C# y a veces diseño en 3D. Estoy pensando seriamente en abrir un estudio con mis amigos...</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-3">
+                    <div class="well">
+                        <p>Alex</p>
+                        <img src="../Imagenes/Destiny2.jpg" class="img-circle" height="55" width="55" alt="Avatar">
+                    </div>
+                </div>
+                <div class="col-sm-9">
+                    <div class="well">
+                        <p>Soy programador y diseñador digital profesional. Mi especialidad es crear videojuegos en C# y a veces diseño en 3D. Estoy pensando seriamente en abrir un estudio con mis amigos...</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-2 well">
+            <div class="thumbnail">
+                <p>Eventos por venir:</p>
+                <img src="../Imagenes/Abraham%20Isaí%20Ramirez%20Gaona-Sombras%20(2).jpg" alt="Paris" width="400" height="300">
+                <p><strong>Paris</strong></p>
+                <p>Viernes. 27 Octubre 2017</p>
+                <button class="btn btn-primary">Info</button>
+            </div>
+            <div class="well">
+                <p>ADS</p>
+            </div>
+            <div class="well">
+                <p>ADS</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<footer class="container-fluid text-center">
+    <p>Texto del pie de pagina</p>
+</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>window.jQuery || document.write('<script src="../../../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+<script src="../sources/js/bootstrap.min.js"></script>
+</body>
